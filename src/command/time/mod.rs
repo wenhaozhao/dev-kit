a607e17a71500690f46d5663f845ab0d5fbe550d
wenhaozhao <wenhaozhao@sohu-inc.com>
2025-12-26 15:56:58 +0800

    update timestring_guess.rs

--- src/command/time/mod.rs
+++ src/command/time/mod.rs
@@ -1,5 +1,5 @@
 use anyhow::anyhow;
-use chrono::Utc;
+use chrono::{FixedOffset, Utc};
 use derive_more::{Deref, Display, From};
 use std::io::Read;
 use std::str::FromStr;
@@ -9,105 +9,36 @@ pub enum TimeCommand {
     #[clap(about = "get current time")]
     Now {
         #[arg(long, short, help = "timezone, alias tz, default to LOCAL", alias = "tz")]
-        timezone: Option<chrono::FixedOffset>,
+        timezone: Option<FixedOffset>,
         #[arg(long, short, help = "time format: rfc3339(default), timestamp(ts) or custom format")]
         format: Option<TimeFormat>,
     },
-    #[clap(about = "parse timestamp")]
-    Timestamp {
-        #[arg(help = "timestamp to parse", default_value = "-")]
-        time: Timestamp,
-        #[arg(short, long, help = "time unit, seconds(s) or milliseconds(ms, default)")]
+    #[clap(about = "time paser")]
+    Parse {
+        #[arg(help = "time to parse, support unix-timestamp or string time, eg. 2023-01-01 12:00:00", default_value = "-")]
+        time: Time,
+        #[arg(short, long, help = "unix-timestamp unit, seconds(s) or milliseconds(ms, default)")]
         unit: Option<TimeUnit>,
-        #[arg(long, short, help = "timezone, alias tz, default to LOCAL", alias = "tz")]
-        timezone: Option<chrono::FixedOffset>,
-        #[arg(long, short, help = "time format: rfc3339(default), timestamp(ts) or custom format")]
-        format: Option<TimeFormat>,
-    },
-    #[clap(about = "parse timestring")]
-    Timestring {
-        #[arg(help = "time string to parse, eg. 2023-01-01 12:00:00, or default stdin with '-'", default_value = "-")]
-        time: StringTime,
-        #[arg(long, short, help = "timezone, alias tz, default to LOCAL", alias = "tz")]
-        timezone: Option<chrono::FixedOffset>,
+        #[arg(long, short, help = "timezone, alias tz, default to local", alias = "tz")]
+        timezone: Option<FixedOffset>,
         #[arg(long, short, help = "time format: rfc3339(default), timestamp(ts) or custom format")]
         format: Option<TimeFormat>,
     },
 }
 
-impl super::Command for TimeCommand {
-    fn run(&self) -> crate::Result<()> {
-        match self {
-            TimeCommand::Now { timezone, format } => {
-                let timezone = timezone.unwrap_or(*chrono::Local::now().offset());
-                let format = format.clone().unwrap_or_default();
-                let time = chrono::Local::now().with_timezone(&timezone);
-                let result = match format {
-                    TimeFormat::RFC3339 => time.to_rfc3339(),
-                    TimeFormat::Timestamp => time.timestamp_millis().to_string(),
-                    TimeFormat::Format(format) => time.format(&format).to_string(),
-                };
-                println!("{result}");
-                Ok(())
-            }
-            TimeCommand::Timestamp { time, unit, timezone, format, } => {
-                let unit = unit.unwrap_or_default();
-                let timezone = timezone.unwrap_or(*chrono::Local::now().offset());
-                let time = match unit {
-                    TimeUnit::Seconds => chrono::DateTime::from_timestamp(**time, 0),
-                    TimeUnit::Milliseconds => chrono::DateTime::from_timestamp_millis(**time),
-                }.ok_or(anyhow!("Invalid timestamp {}{}", time, unit))?.with_timezone(&timezone);
-                let format = format.clone().unwrap_or_default();
-                let result = match format {
-                    TimeFormat::RFC3339 => time.to_rfc3339(),
-                    TimeFormat::Timestamp => time.timestamp_millis().to_string(),
-                    TimeFormat::Format(format) => time.format(&format).to_string(),
-                };
-                println!("{result}");
-                Ok(())
-            }
-            TimeCommand::Timestring { time, timezone, format, } => {
-                let timezone = timezone.unwrap_or(*chrono::Local::now().offset());
-                let time = chrono::DateTime::<Utc>::from_str(time).map_err(|err| {
-                    log::debug!("Failed to parse time string: {}, error: {}", time, err);
-                    anyhow!("Invalid string time {time}")
-                })?.with_timezone(&timezone);
-                let format = format.clone().unwrap_or_default();
-                let result = match format {
-                    TimeFormat::RFC3339 => time.to_rfc3339(),
-                    TimeFormat::Timestamp => time.timestamp_millis().to_string(),
-                    TimeFormat::Format(format) => time.format(&format).to_string(),
-                };
-                println!("{result}");
-                Ok(())
-            }
-        }
-    }
-}
 
+#[derive(Debug, Clone, Display)]
+pub enum Time {
+    StringTime(Timestring),
+    Timestamp(Timestamp),
+}
+#[derive(Debug, Clone, Display, Deref, From)]
+#[display("{_0}")]
+pub struct Timestring(String);
+mod timestring_guess;
 #[derive(Debug, Copy, Clone, Display, Deref, From)]
 #[display("{_0}")]
 pub struct Timestamp(i64);
-
-impl FromStr for Timestamp {
-    type Err = anyhow::Error;
-
-    fn from_str(value: &str) -> Result<Self, Self::Err> {
-        let val: i64 = if value.eq("-") {
-            let mut string = String::new();
-            let _ = std::io::stdin().lock().read_to_string(&mut string)
-                .map_err(|err| anyhow!("read from stdin failed, {}", err))?;
-            string.trim().parse().map_err(|err|
-                anyhow!("Invalid timestamp: {}, {}", value, err)
-            )?
-        } else {
-            value.trim().parse().map_err(|err|
-                anyhow!("Invalid timestamp: {}, {}", value, err)
-            )?
-        };
-        Ok(Timestamp(val))
-    }
-}
 
 #[derive(Debug, Copy, Clone, Display, Default)]
 pub enum TimeUnit {
@@ -116,6 +47,81 @@ pub enum TimeUnit {
     #[default]
     #[display("ms")]
     Milliseconds,
+}
+
+#[derive(Debug, Clone, Default)]
+pub enum TimeFormat {
+    #[default]
+    RFC3339,
+    Timestamp,
+    Format(String),
+}
+
+impl super::Command for TimeCommand {
+    fn run(&self) -> crate::Result<()> {
+        match self {
+            TimeCommand::Now { timezone, format } => {
+                let timezone = timezone.unwrap_or(*chrono::Local::now().offset());
+                let time = chrono::Local::now().with_timezone(&timezone);
+                let result = Self::time_formatter(&time, &format)?;
+                println!("{result}");
+                Ok(())
+            }
+            TimeCommand::Parse { time, unit, timezone, format, } => {
+                let unit = unit.unwrap_or_default();
+                let timezone = timezone.unwrap_or(*chrono::Local::now().offset());
+                let time = match time {
+                    Time::Timestamp(time) => {
+                        match unit {
+                            TimeUnit::Seconds => chrono::DateTime::from_timestamp(**time, 0),
+                            TimeUnit::Milliseconds => chrono::DateTime::from_timestamp_millis(**time),
+                        }.ok_or(anyhow!("Invalid timestamp {}{}", time, unit))?.with_timezone(&timezone)
+                    }
+                    Time::StringTime(time) => {
+                        chrono::DateTime::<Utc>::try_from(time).map_err(|err| {
+                            log::debug!("Failed to parse time string: {}, error: {}", time, err);
+                            anyhow!("Invalid string time {time}")
+                        })?.with_timezone(&timezone)
+                    }
+                };
+                let result = Self::time_formatter(&time, &format)?;
+                println!("{result}");
+                Ok(())
+            }
+        }
+    }
+}
+
+impl TimeCommand {
+    fn time_formatter(time: &chrono::DateTime<FixedOffset>, format: &Option<TimeFormat>) -> crate::Result<String> {
+        let format = format.clone().unwrap_or_default();
+        let result = match format {
+            TimeFormat::RFC3339 => time.to_rfc3339(),
+            TimeFormat::Timestamp => time.timestamp_millis().to_string(),
+            TimeFormat::Format(format) => time.format(&format).to_string(),
+        };
+        Ok(result)
+    }
+}
+
+impl FromStr for Time {
+    type Err = anyhow::Error;
+
+    fn from_str(value: &str) -> Result<Self, Self::Err> {
+        let val = if value.eq("-") {
+            let mut string = String::new();
+            let _ = std::io::stdin().lock().read_to_string(&mut string)
+                .map_err(|err| anyhow!("read from stdin failed, {}", err))?;
+            string.trim().to_string()
+        } else {
+            value.to_string()
+        };
+        if let Ok(val) = value.parse::<i64>() {
+            Ok(Self::Timestamp(val.into()))
+        } else {
+            Ok(Self::StringTime(val.into()))
+        }
+    }
 }
 
 impl FromStr for TimeUnit {
@@ -129,34 +135,6 @@ impl FromStr for TimeUnit {
             _ => Err(anyhow!("Invalid time unit: {}", s)),
         }
     }
-}
-
-#[derive(Debug, Clone, Display, Deref)]
-#[display("{_0}")]
-pub struct StringTime(String);
-mod string_time_parser;
-
-impl FromStr for StringTime {
-    type Err = anyhow::Error;
-
-    fn from_str(value: &str) -> Result<Self, Self::Err> {
-        if value.eq("-") {
-            let mut string = String::new();
-            let _ = std::io::stdin().lock().read_to_string(&mut string)
-                .map_err(|err| anyhow!("read from stdin failed, {}", err))?;
-            Ok(StringTime(string.trim().to_string()))
-        } else {
-            Ok(StringTime(value.trim().to_string()))
-        }
-    }
-}
-
-#[derive(Debug, Clone, Default)]
-pub enum TimeFormat {
-    #[default]
-    RFC3339,
-    Timestamp,
-    Format(String),
 }
 
 impl FromStr for TimeFormat {
--- src/command/time/timestring_guess.rs
+++ src/command/time/timestring_guess.rs
@@ -1,11 +1,11 @@
-use super::StringTime;
+use super::Timestring;
 use chrono::TimeZone;
 use std::convert::TryFrom;
 
-impl TryFrom<&StringTime> for chrono::DateTime<chrono::Utc> {
+impl TryFrom<&Timestring> for chrono::DateTime<chrono::Utc> {
     type Error = anyhow::Error;
 
-    fn try_from(StringTime(val): &StringTime) -> Result<Self, Self::Error> {
+    fn try_from(Timestring(val): &Timestring) -> Result<Self, Self::Error> {
         parse_from_rfc3339(val)
             .or_else(|| parse_from_rfc2822(val))
             .or_else(|| guess_from_known_formats(val))
@@ -36,18 +36,35 @@ fn parse_from_rfc2822(val: &str) -> Option<chrono::DateTime<chrono::Utc>> {
 
 fn guess_from_known_formats(val: &str) -> Option<chrono::DateTime<chrono::Utc>> {
     const FORMATES: &[&str] = &[
+        "%a %b %d %H:%M:%S %Z %Y",
         "%Y-%m-%d %H:%M:%S",
-        "%Y/%m/%d %H:%M:%S",
         "%Y-%m-%dT%H:%M:%S",
         "%Y-%m-%d %H:%M:%S%.f",
-        "%Y/%m/%d %H:%M:%S%.f",
         "%Y-%m-%dT%H:%M:%S%.f",
+        "%Y/%m/%d %H:%M:%S",
+        "%Y/%m/%dT%H:%M:%S",
+        "%Y/%m/%d %H:%M:%S%.f",
+        "%Y/%m/%dT%H:%M:%S%.f",
+        "%m-%d-%Y %H:%M:%S",
+        "%m-%d-%YT%H:%M:%S",
+        "%m-%d-%Y %H:%M:%S%.f",
+        "%m-%d-%YT%H:%M:%S%.f",
+        "%m/%d/%Y %H:%M:%S",
+        "%m/%d/%YT%H:%M:%S",
+        "%m/%d/%Y %H:%M:%S%.f",
+        "%m/%d/%YT%H:%M:%S%.f",
+        "%d-%m-%Y %H:%M:%S",
+        "%d-%m-%YT%H:%M:%S",
+        "%d-%m-%Y %H:%M:%S%.f",
+        "%d-%m-%YT%H:%M:%S%.f",
+        "%d/%m/%Y %H:%M:%S",
+        "%d/%m/%YT%H:%M:%S",
+        "%d/%m/%Y %H:%M:%S%.f",
+        "%d/%m/%YT%H:%M:%S%.f",
         "%Y-%m-%d",
         "%Y/%m/%d",
-        "%d-%m-%Y %H:%M:%S",
-        "%d/%m/%Y %H:%M:%S",
-        "%m-%d-%Y %H:%M:%S",
-        "%m/%d/%Y %H:%M:%S",
+        "%m-%d-%Y",
+        "%d-%m-%Y",
     ];
     let default_timezone = *chrono::Local::now().offset();
     for &format in FORMATES {
