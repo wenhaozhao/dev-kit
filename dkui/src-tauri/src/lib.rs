c07c07e6f65454215bf70a8e91e4fa22c6274646
wenhaozhao <wenhaozhao@sohu-inc.com>
2026-01-05 14:15:58 +0800

    add support for adding `devkit` to PATH and related UI improvements

--- dkui/src-tauri/src/lib.rs
+++ dkui/src-tauri/src/lib.rs
@@ -2,9 +2,14 @@ use chrono::FixedOffset;
 use dev_kit as devkit;
 use devkit::command::json::{DiffTool, Json};
 use devkit::command::time::{Time, TimeCommand, TimeFormat, TimestampUnit};
+use lazy_static::lazy_static;
 use std::collections::HashMap;
+use std::io::Write;
+use std::path::PathBuf;
 use std::str::FromStr;
+use std::sync::atomic::{AtomicPtr, Ordering};
 use std::sync::{Arc, Mutex};
+use std::{fs, ptr};
 
 #[derive(Default)]
 struct JsonCache {
@@ -179,6 +184,90 @@ fn save_to_file(path: String, content: String) -> Result<(), String> {
     std::fs::write(&path, content).map_err(|e| e.to_string())
 }
 
+lazy_static! {
+    static ref DEVKIT_PATH: AtomicPtr<PathBuf> = {
+        match which::which("devkit"){
+            Ok(path) => {
+                let ptr = Box::into_raw( Box::new(path));
+                AtomicPtr::new(ptr)
+            },
+            Err(_) => {
+                AtomicPtr::new(ptr::null_mut())
+            }
+        }
+    };
+}
+#[tauri::command]
+fn show_add_to_path_bth() -> Result<String, String> {
+    let devkit_path_ptr = DEVKIT_PATH.load(Ordering::Relaxed);
+    if devkit_path_ptr.is_null() {
+        #[cfg(any(target_os = "macos", target_os = "linux"))]
+        {
+            return Ok("".to_string());
+        }
+        #[cfg(target_os = "windows")]{
+            return Err("Unsupported operating system".to_string());
+        }
+    } else {
+        let path = unsafe { &*devkit_path_ptr };
+        Ok(path.display().to_string())
+    }
+}
+
+fn get_current_shell() -> Option<String> {
+    std::env::var("SHELL").ok().and_then(|shell_path| {
+        PathBuf::from(&shell_path)
+            .file_name()
+            .and_then(|name| name.to_str())
+            .map(|s| s.to_string())
+    })
+}
+
+#[tauri::command]
+fn add_to_path() -> Result<String, String> {
+    if  !DEVKIT_PATH.load(Ordering::Relaxed).is_null() {
+        return Ok("devkit already in PATH".to_string());
+    }
+    #[cfg(any(target_os = "macos", target_os = "linux"))]{
+        let current_exe = std::env::current_exe().map_err(|e| e.to_string())?;
+        let bin_dir = current_exe.parent().ok_or("Failed to get bin directory")?;
+        let home_path = PathBuf::from(std::env::var("HOME").map_err(|e| e.to_string())?);
+        let shell = get_current_shell().ok_or("Failed to get current shell")?.to_lowercase();
+        match shell.as_str() {
+            "fish" => {
+                let fish_config_path = home_path.join(".config/fish/conf.d/devkit.fish");
+                let mut file = fs::File::options().create(true).append(true).open(&fish_config_path).map_err(|e| e.to_string())?;
+                file.write(format!(r#"
+set -x PATH {} $PATH
+alias dk "devkit"
+                "#, bin_dir.display()).as_bytes()).map_err(|e| e.to_string())?;
+            }
+            "zsh" => {
+                let zsh_config_path = home_path.join(".zshrc");
+                let mut file = fs::File::options().create(true).append(true).open(&zsh_config_path).map_err(|e| e.to_string())?;
+                file.write(format!(r#"
+export PATH={}:$PATH
+alias dk="devkit"
+                "#, bin_dir.display()).as_bytes()).map_err(|e| e.to_string())?;
+            }
+            "bash" => {
+                let bash_config_path = home_path.join(".bashrc");
+                let mut file = fs::File::options().create(true).append(true).open(&bash_config_path).map_err(|e| e.to_string())?;
+                file.write(format!(r#"
+export PATH={}:$PATH
+alias dk="devkit"
+                "#, bin_dir.display()).as_bytes()).map_err(|e| e.to_string())?;
+            }
+            _ => {}
+        }
+        DEVKIT_PATH.store(Box::into_raw(Box::new(bin_dir.join("devkit"))), Ordering::Relaxed);
+        return Ok("add devkit to PATH ok, you can use devkit or dk in command-line now!!!".to_string());
+    }
+    #[cfg(target_os = "windows")]{
+        return Err("Unsupported operating system".to_string());
+    }
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
@@ -196,7 +285,9 @@ pub fn run() {
             diff_json,
             get_available_diff_tools,
             parse_time,
-            save_to_file
+            save_to_file,
+            show_add_to_path_bth,
+            add_to_path,
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
--- dkui/src/App.vue
+++ dkui/src/App.vue
@@ -1,6 +1,8 @@
 <script setup>
-import { ref } from "vue";
+import {onMounted, ref} from "vue";
 import { openUrl } from "@tauri-apps/plugin-opener";
+import { invoke } from "@tauri-apps/api/core";
+import { message } from "@tauri-apps/plugin-dialog";
 import JsonParser from "./components/JsonParser.vue";
 import JsonDiff from "./components/JsonDiff.vue";
 import UriParser from "./components/UriParser.vue";
@@ -12,8 +14,31 @@ const jsonRightInput = ref("");
 const jsonQuery = ref("");
 const currentTab = ref("json");
 
+const is_show_add_to_path_bth = ref(false);
 const openGithub = async () => {
   await openUrl("https://github.com/wenhaozhao/dev-kit");
+};
+
+const show_add_to_path_bth = async () => {
+  try {
+    const res = await invoke("show_add_to_path_bth");
+    is_show_add_to_path_bth.value = typeof res === 'string' && res.length === 0;
+  } catch (err) {
+  }
+}
+
+onMounted(() => {
+  show_add_to_path_bth();
+});
+
+const addToPath = async () => {
+  try {
+    const res = await invoke("add_to_path");
+    await message(res, { title: 'Success', kind: 'info' });
+    await show_add_to_path_bth()
+  } catch (err) {
+    await message(err, { title: 'Error', kind: 'error' });
+  }
 };
 
 function updateJson(val) {
@@ -37,6 +62,11 @@ function updateQuery(val) {
       <button :class="{ active: currentTab === 'uri_parse' }" @click="currentTab = 'uri_parse'">URI Parser</button>
       <button :class="{ active: currentTab === 'uri_decoder' }" @click="currentTab = 'uri_decoder'">URI Decoder</button>
       <button :class="{ active: currentTab === 'time' }" @click="currentTab = 'time'">Time Parser</button>
+      <a href="#" class="github-link" @click.prevent="addToPath" title="Add devkit(dk) to Path" v-if="is_show_add_to_path_bth">
+        <svg height="24" viewBox="0 0 24 24" width="24" fill="currentColor">
+          <path d="M20,19V7H4V19H20M20,3A2,2 0 0,1 22,5V19A2,2 0 0,1 20,21H4A2,2 0 0,1 2,19V5C2,3.89 2.9,3 4,3H20M13,17V15H18V17H13M9.58,13L5.57,9H8.4L11.7,12.3C12.09,12.69 12.09,13.33 11.7,13.72L8.42,17H5.59L9.58,13Z" />
+        </svg>
+      </a>
       <a href="#" class="github-link" @click.prevent="openGithub" title="GitHub">
         <svg height="24" viewBox="0 0 16 16" width="24" fill="currentColor">
           <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
@@ -105,12 +135,15 @@ function updateQuery(val) {
 }
 
 .github-link {
-  margin-left: auto;
   display: flex;
   align-items: center;
   color: #333;
   transition: color 0.2s;
   padding: 0 10px;
+}
+
+.github-link:first-of-type {
+  margin-left: auto;
 }
 
 .github-link:hover {
